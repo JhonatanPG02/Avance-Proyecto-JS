fa6635ae8f2eb7dcb2baef7ce42dfef6af54665a
JhonatanPG02 <jhonatan.prado.gutierrez@gmail.com>
2022-04-13 23:24:22 -0500

    Resuelto

--- miarchivo.js
+++ miarchivo.js
@@ -1,109 +1,171 @@
 // Realizaremos un pedido de combo: Hamburguesa + Bebida
 
+
 let nombre;
 
-// Aplicaremos un while para descartar ingreso de numeros o cancele la solicitud
-while(nombre == undefined || nombre == parseInt(nombre)) {
+// Aplicaremos un Do while para descartar ingreso de numeros o datos vacios.
 
+do {
     nombre = prompt("Ingrese su nombre")
-    if (nombre == undefined || nombre == parseInt(nombre)){
+    if (nombre === "" || nombre === null){
         alert(`ERROR! debe ingresar un nombre valido`);
     } else {
         alert(`Bienvenido a la tienda REY BURGER online ${nombre}! \n
-        Vamos a tomar el pedido de tu combo`);
+        Vamos a tomar el pedido de tu combo`)
     }
-}
+} while(nombre === "" || nombre === null) 
+
+
+// Incluiremos 2 Array de objetos, con los valores de las Hamburguesas y Bebidas, además de ID, precio y tamaño.
+// Esto permitirá dar opciones al cliente y tener datos necesarios para aplicar precio y generar el ticket.
+
+
+const totalBurger = [{ID: 1, Burger: "clasica", Price: "8", Size: "mediano"},
+                     {ID: 2, Burger: "clasica", Price: "10", Size: "grande"},
+                     {ID: 3, Burger: "royal", Price: "10", Size: "mediano"},
+                     {ID: 4, Burger: "royal", Price: "14", Size: "grande"},
+                     {ID: 5, Burger: "suprema", Price: "16", Size: "mediano"},
+                     {ID: 6, Burger: "suprema", Price: "20", Size: "grande"}]
 
 
 
-let hamburguesa;
+
+
+const totalDrink = [{ID: 1, Drink: "coca cola", Price: "3", Size: "mediano"},
+                    {ID: 2, Drink: "coca cola", Price: "5", Size: "grande"},
+                    {ID: 3, Drink: "pepsi", Price: "2", Size: "mediano"},
+                    {ID: 4, Drink: "pepsi", Price: "4", Size: "grande"},
+                    {ID: 5, Drink: "fanta", Price: "1.5", Size: "mediano"},
+                    {ID: 6, Drink: "fanta", Price: "3", Size: "grande"}]
+
+let burger;
+let drink;
+
+
+let burgerSize;
+
+// Aplicaremos un Do while para solicitar el tipo de tamaño de hamburguesa, con la finalidad de aplicar filter más adelante.
+
+do {
+    burgerSize = prompt(`Indique el tamaño de su Hamburguesa: "Mediano" o "Grande"`)
+
+    if(burgerSize) burgerSize = burgerSize.toLowerCase();
+
+    if ((burgerSize != "mediano" && burgerSize != "grande") || burgerSize === "" || burgerSize === null){
+        alert(`ERROR! debe ingresar un tamaño valido`);
+    } else {
+        alert(`Gracias, el siguiente paso es elegir el tipo de hamburguesa`)
+    }
+} while((burgerSize != "mediano" && burgerSize != "grande") || burgerSize === "" || burgerSize === null)
+
+
+
 
 // Armaremos la funcion Hamburguesa para luego invocarla.
 function pedidoHamburguesa(){
 
-   alert(`Paso 1: Vamos a tomar el pedido de tu Hamburguesa \n
-   Tienes las siguientes opciones: \n
-   * Hamburguesa Clasica = 8$ \n
-   * Hamburguesa Royal = 10$ \n
-   * Hamburguesa Suprema = 15$`)
-// Si el pedido es distinto a las 3 opciones o cancela el promp, se repite el bucle
-    while ((hamburguesa != "clasica" && hamburguesa != "royal" && hamburguesa != "suprema") || (hamburguesa == undefined)) {
-        hamburguesa = prompt(`Ingrese una de las 3 disponibles: "Clasica", "Royal", "Suprema"`);
+   
+//Aplicaremos el Do while, si el pedido es distinto a las 3 opciones o no ingresa dato/cancela, se repite el bucle
+
+do {
+    burger = prompt(`Ingrese una de las 3 disponibles: "Clasica", "Royal", "Suprema"`);
     
-        if(hamburguesa) hamburguesa = hamburguesa.toLowerCase()
-    
-    
-        if((hamburguesa != "clasica" && hamburguesa != "royal" && hamburguesa != "suprema") || (hamburguesa == undefined)) {
-            alert(`ERROR! debes elegir entre las 3 opciones`);
-        } else {  
-            alert(`El pedido de tu hamburguesa es una "${hamburguesa}"`);
-        }
+    if(burger) burger = burger.toLowerCase()
+
+    if((burger != "clasica" && burger != "royal" && burger != "suprema") || burger === "" || burger === null) {
+        alert(`ERROR! debes elegir entre las 3 opciones`);
+    } else {  
+        
+        alert(`El pedido de tu hamburguesa es una "${burger}"`);
+    }
+} while ((burger != "clasica" && burger != "royal" && burger != "suprema") || burger === "" || burger === null)
+       
 }
-}
+
 
 pedidoHamburguesa();
 
 
-let bebida;
+
+let drinkSize;
+
+// Aplicaremos un Do while para solicitar el tipo de tamaño de Bebida, con la finalidad de aplicar filter más adelante.
+
+
+do {
+    drinkSize = prompt(`Indique el tamaño de su Bebida: "Mediano" o "Grande"`)
+
+    if(drinkSize) drinkSize = drinkSize.toLowerCase()
+
+    if ((drinkSize != "mediano" && drinkSize != "grande") || drinkSize === "" || drinkSize === null){
+        alert(`ERROR! debe ingresar un tamaño valido`);
+    } else {
+        alert(`Gracias, el siguiente paso es elegir el tipo de bebida`)
+    }
+} while((drinkSize != "mediano" && drinkSize != "grande") || drinkSize === "" || drinkSize === null)
+
+
+
+
 
 // Armaremos la funcion Bebida para luego invocarla.
 function pedidoBebida(){
 
-    alert(`Paso 2: Vamos a tomar el pedido de tu Bebida \n
-    Tienes las siguientes opciones: \n
-    * Bebida Coca Cola = 3$ \n
-    * Bebida Pepsi = 2$`)
-// Si el pedido es distinto a las 2 opciones o cancela el promp, se repite el bucle
-    while ((bebida != "coca cola" && bebida != "pepsi") || (bebida == undefined)) {
-        bebida = prompt(`Ingrese una de las 2 disponibles: "Coca Cola", "Pepsi"`)
-
-        if(bebida) bebida = bebida.toLowerCase()
     
-        if ((bebida != "coca cola" && bebida != "pepsi") || (bebida == undefined)){
-            alert(`ERROR! debes elegir entre las 2 bebidas`);
+// Aplicaremos un Do while, si el pedido es distinto a las 3 opciones o no ingresa dato/cancela, se repite el bucle
+
+    do{
+        drink = prompt(`Ingrese una de las 2 disponibles: "Coca Cola", "Pepsi", "Fanta"`);
+    
+        if(drink) drink = drink.toLowerCase();
+
+        if ((drink != "coca cola" && drink != "pepsi" && drink != "fanta") || (drink === "")){
+            alert(`ERROR! debes elegir entre las 3 bebidas`);
         } else {
-            alert(`El pedido de tu bebida es una "${bebida}"`)
+            alert(`El pedido de tu bebida es una "${drink}"`)
         }
-    }
+    } while ((drink != "coca cola" && drink != "pepsi" && drink != "fanta") || (drink === ""))
+    
     }
 
     pedidoBebida();
 
-    // Calculando el precio del combo solicitado.
-  
-    let precioHamburguesa = 0;
-    let precioBebida = 0;
+    let ticket = []
+    
 
+    function precioProducto(){
 
-    function precioTotal(){
-         
-        if(hamburguesa == "clasica") {
-            precioHamburguesa = 8
-            alert(`El precio de tu Hamburguesa ${hamburguesa} es de: ${precioHamburguesa}$`)
-        } else if (hamburguesa == "royal") {
-            precioHamburguesa = 10
-            alert(`El precio de tu Hamburguesa ${hamburguesa} es de: ${precioHamburguesa}$`)
-        } else if (hamburguesa == "suprema"){
-            precioHamburguesa = 15
-            alert(`El precio de tu Hamburguesa ${hamburguesa} es de: ${precioHamburguesa}$`)
+  // Para el precio, aplicaremos un for para determinar la concidencia del valor Hamburguesa/refreso y obtener su precio.
+  // Aplicaremos el metodo "FILTER" para obtener nuevo array con los productos según tamaño que el ciente eligió y aplicar precio.
+  // Aplicaremos también el metodo "FIND" para obtener solo el objeto segun el ID del nuevo array obtenido por FILTER.
+  // Por último aplicaremos el metodo "PUSH" para agregar los objetos de hamburguesa y refresco en nuevo array que será el ticket.
+  // Estamos aplicando el metodo length para la validacion del dato requerido en toda su lengitud del array de objetos.
+
+    let typeBurger = totalBurger.filter(Element => Element.Size === burgerSize)
+
+        for(let i=0; i<typeBurger.length; i++) {
+            if(typeBurger[i].Burger === burger) {
+                alert(`El precio de tu Hamburguesa ${burger} ${burgerSize} es de: ${typeBurger[i].Price}$`)
+                let burgerTicket = typeBurger.find((el) => el.ID === typeBurger[i].ID)
+                ticket.push(burgerTicket)
+         }
         }
 
-        if(bebida == "coca cola") {
-        precioBebida = 3
-        alert(`El precio de tu Bebida ${bebida} es de: ${precioBebida}$`)
-        } else if (bebida == "pepsi") {
-        precioBebida = 2
-        alert(`El precio de tu Bebida ${bebida} es de: ${precioBebida}$`)  
-        }
+        let typeDrink = totalDrink.filter(Element => Element.Size === drinkSize)
 
-        let precioTotal = precioHamburguesa + precioBebida
-        alert(`El precio total de tu combo es de ${precioTotal}$`)
-        alert(`Gracias por tu compra, que disfrutes tu pedido!`)
-    }
+        for(let i=0; i<typeDrink.length; i++) {
+            if(typeDrink[i].Drink === drink) {
+                alert(`El precio de tu Bebida ${drink} ${drinkSize} es de: ${typeDrink[i].Price}$`)
+                let drinkTicket = typeDrink.find((el) => el.ID === typeDrink[i].ID)
+                ticket.push(drinkTicket)
+            }
+        }
+    
+        alert(`Gracias por tu compra!`) 
+    return ticket    
+}
 
     
-   
-    precioTotal();
-
-
+precioProducto();
+console.log(ticket);
 
